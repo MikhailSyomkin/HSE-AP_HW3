33bf80a173ab20041951d974407896b81edca570
MikhailSyomkin <semkin.m.d@gmail.com>
2025-03-24 00:17:50 +0300

    Create auth.py

---
+++ services/auth.py
@@ -0,0 +1,26 @@
+from datetime import datetime, timedelta
+from typing import Union
+from jose import JWTError, jwt
+from app import schemas
+
+# Конфигурация
+SECRET_KEY = "your_secret_key"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+# Создание JWT токена
+def create_access_token(data: dict, expires_minutes: int = ACCESS_TOKEN_EXPIRE_MINUTES):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=expires_minutes)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+# Верификация JWT токена
+def verify_token(token: str) -> Union[schemas.UserResponse, None]:
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        user = schemas.UserResponse(**payload)  # Создание пользователя из токена
+        return user
+    except JWTError:
+        return None
